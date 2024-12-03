99f3433ba5c9e9b9b7d603ffc0c8581936fd8381
pkenway <pkenway@gmail.com>
2024-12-03 10:01:50 -0800

    add css

--- init.lua
+++ init.lua
@@ -581,6 +581,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        cssls = {},
         --
 
         lua_ls = {
